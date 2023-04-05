07548559cf409994ce3ca35260953a6a5a0b1b63
Vuong pham <vuongpham@gmail.com>
2023-04-05 14:51:18 +0700

    feature\database_design

---
+++ ShopOnlineSmart.Data/EF/ShopOnlineDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopOnlineSmart.Data.EF
+{
+    public class ShopOnlineDbContext : DbContext
+    {
+        public ShopOnlineDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet
+    }
+}
