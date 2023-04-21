73922296e7ee21a6f66ef0d88f8858a5018b7d3f
Vuong pham <vuongpham@gmail.com>
2023-04-22 01:35:58 +0700

    fix

---
+++ ShopOnlineSmart.WebApp/LoginDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOnlineSmart.WebApp.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopOnlineSmart.WebApp.Models
+{
+    public class LoginDbContext : DbContext
+    {
+        //public LoginDbContext(DbContextOptions/*<ShopOnlineDbContext>*/ options) : base(options)
+        //{
+        //}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=VUONGPHAM\\PHAMXUANVUONG;Database=ShopOnlineSmart;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+        public DbSet<Login> Logins { get; set; }
+
+        
+    }
+}
